29175fb0a9c69ab4e94fb4719a92dc9a74002a6a
Kyle Austad <kyleaustad@gmail.com>
2025-09-18 10:29:27 -0500

    use Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -107,6 +108,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased dark`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
